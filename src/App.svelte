87e5698b7104e022f6159ac787687430f0bb6a9a
Sevan KABAKIAN <kabakian@cirad.fr>
2024-08-28 15:55:05 +0200

    [FEAT] - SNT6 - Loops (also added ts)

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     // export let name;
     let firstName = "Jimi";
     let lastName = "Hendrix";
@@ -9,6 +9,19 @@
 
     // reactive statement
     $: console.log(beltColor);
+
+    // data for the loop
+    interface Nakamma {
+        name: String;
+        role: String;
+        id: Number;
+    }
+
+    let crewMembers: Nakamma[] = [
+        { name: "Luffy", role: "captain", id: 1 },
+        { name: "Zoro", role: "swordsman", id: 2 },
+        { name: "Nami", role: "navigator", id: 3 },
+    ];
 </script>
 
 <main>
@@ -18,6 +31,21 @@
         <input type="text" bind:value={lastName} />
         <input type="text" bind:value={beltColor} />
     </div>
+
+    <!-- Loop exemple -->
+    <section>
+        <h3>Strawhat crew nakama's</h3>
+        <article class="card-container">
+            {#each crewMembers as nakama (nakama.id) }
+                <div class="card">
+                    <h4>{nakama.name}</h4>
+                    <p>{nakama.role} of the crew !</p>
+                </div>
+                {:else}
+                <p>There ar no nakama to display...</p>
+            {/each}
+        </article>
+    </section>
 </main>
 
 <style>
@@ -26,13 +54,6 @@
         padding: 1em;
         max-width: 240px;
         margin: 0 auto;
-    }
-
-    h1 {
-        color: #ff3e00;
-        text-transform: uppercase;
-        font-size: 4em;
-        font-weight: 100;
     }
 
     @media (min-width: 640px) {
@@ -45,5 +66,25 @@
         display: flex;
         flex-direction: column;
         align-items: center;
+    }
+
+    .card-container {
+        display: flex;
+        justify-content: space-around;
+        margin: auto;
+        max-width: 80%;
+        flex-wrap: wrap;
+    }
+
+    .card {
+        background: rgba(240, 128, 128, 0.3);
+        flex: 1;
+        padding: 1rem;
+        margin: 0 2rem;
+        border-radius: 0.5rem;
+        box-shadow: 0.25rem 0.25rem 0.25rem rgba(211, 211, 211, 0.9);
+        &:hover {
+            box-shadow: 0.5rem 0.5rem 0.5rem rgba(211, 211, 211, 0.9);
+        }
     }
 </style>
